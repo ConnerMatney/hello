613a7347c5799af6ccc314364c57260de1a357fb
ConnerMatney <cmatney2@huskers.unl.edu>
2020-08-20 00:02:04 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Conner Matney
+ *         cmatney2@huskers.unl.edu
+ * Date: 2020/08/17
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Conner Matney\n");
+
+  return 0;
+}
